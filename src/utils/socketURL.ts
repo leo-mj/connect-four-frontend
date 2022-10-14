219bd75637013cc4292fad5c9c38b26f268cb6eb
Leo <leo.maedje@gmail.com>
2022-10-14 15:52:08 +0100

    Changed socketURL

--- src/utils/socketURL.ts
+++ src/utils/socketURL.ts
@@ -1,4 +1,4 @@
 export const socketURL =
   process.env.NODE_ENV === "production"
-    ? "https://lm-wordle-platform.herokuapp.com"
+    ? "https://connect-2-connect-4.herokuapp.com"
     : "http://localhost:4000";
