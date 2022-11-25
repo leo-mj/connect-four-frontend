5511a54a924e5e8f1bb4f31e08bd64f750b09f91
Leo <leo.maedje@gmail.com>
2022-11-25 16:15:01 +0000

    Changed back to heroku

--- src/utils/socketURL.ts
+++ src/utils/socketURL.ts
@@ -1,4 +1,4 @@
 export const socketURL =
   process.env.NODE_ENV === "production"
-    ? "https://connect-2-connect-4-backend.onrender.com"
+    ? "https://connect-2-connect-4.herokuapp.com"
     : "http://localhost:4000";
